81573343a2fedd342ca1ab78211e00d28330833a
RoshanSyangtanTamang <rossonsyangtan444@gmail.com>
2025-04-05 20:41:14 +0545

    Adding API

--- script.js
+++ script.js
@@ -4,5 +4,46 @@ const weatherBox = document.querySelector('.weather-box');
 const details = document.querySelector('.weather-details');
 
 search.addEventListener('click', () => {
-    
-})
+    const APIKey = '4c5427391ad44a62c304e459e663f7f4';
+    const city = document.querySelector('.search-box input').value;
+
+    if(city == '')
+        return;
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}$units=metric&appid=${APIKey}').then(response => response.json()).then(json => {
+        const image = document.querySelector('.weather-box img');
+        const temperature = document.querySelector('.weather-box .temp');
+        const description = document.querySelector('.weather-box .desc');
+        const humidity = document.querySelector('.weather-details .humidity span');
+        const wind = document.querySelector('.weather-details .wind span');
+
+        switch (json.weather[0].main) {
+            case 'Clear':
+                image.src = 'sunny.png';
+                break;
+
+            case 'Clouds':
+                image.src = 'partly.png';
+                break;
+
+            case 'Mist':
+                image.src = 'cloudy.png';
+                break;
+
+            case 'Haze':
+                image.src = 'cloudy.png';
+                break;
+
+            case 'Rain':
+                image.src = 'rainy.png';
+                break;
+
+            case 'Snow':
+                image.src = 'snowy.png';
+                break;
+        
+            default:
+                image.src = 'partly.png';
+        }
+    });
+});
