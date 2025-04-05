b576598107096a00ee2b0f8d26c750a0ac29b21b
RoshanSyangtanTamang <rossonsyangtan444@gmail.com>
2025-04-05 16:55:11 +0545

    Adding JS

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const details = document.querySelector('.weather-details');
+
+search.addEventListener('click', () => {
+    
+})
