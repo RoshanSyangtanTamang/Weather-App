3050682c86f02a89901352d84d6c7789232da64c
RoshanSyangtanTamang <rossonsyangtan444@gmail.com>
2025-04-05 21:05:21 +0545

    Fixed API

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ search.addEventListener('click', () => {
     const APIKey = '4c5427391ad44a62c304e459e663f7f4';
     const city = document.querySelector('.search-box input').value;
 
-    if(city == '')
+    if (city == '')
         return;
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}$units=metric&appid=${APIKey}').then(response => response.json()).then(json => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
         const image = document.querySelector('.weather-box img');
         const temperature = document.querySelector('.weather-box .temp');
         const description = document.querySelector('.weather-box .desc');
